bdf6c73b45349a08cce9327665840c052ddba494
Andy Gill <andygill@ku.edu>
2012-04-01 14:57:30 -0500

    Adding to the Main, FTSO building up our quoting.

--- examples/first/Main.hs
+++ examples/first/Main.hs
@@ -12,6 +12,11 @@ main = print (fib 40)
 fib :: Int -> Int
 fib n = if n < 2 then 1 else fib(n-1) + fib (n-2)
 
+example :: Integer
+example = 99
+
+{-
 rewrite_fib :: H
 rewrite_fib =
         consider 'fib []
+-}
