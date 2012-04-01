773d4937ceb2d9274698e270d871d6efe3d60729
Andy Gill <andygill@ku.edu>
2012-04-01 12:49:56 -0500

    Adding some comments to Primitive list.

--- src/Language/HERMIT/Primitive.hs
+++ src/Language/HERMIT/Primitive.hs
@@ -13,7 +13,15 @@ all_prims = Map.unions
         [ fmap promoteR expr_prims
         ]
 
+-- To consider:
+--   should we include small programs, like unroll, as named arguments here?
+--   In this case, these are not primitives, but named rewrites.
+
 expr_prims :: Map String (Rewrite CoreExpr)
 expr_prims = Map.fromList
         [ ("inline", Inline.inline)
+--        , ("eta-expand",...)
+--        , ("eta-reduction",...)
+--        , ("beta-reduction",...)
+--        , ("case-of-known-constructor", ...)
         ]
