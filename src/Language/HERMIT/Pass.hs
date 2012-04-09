f1bab212dabe90d808cd97932b2ffc2f2077dbae
Andy Gill <andygill@ku.edu>
2012-04-08 19:41:46 -0500

    New syntax for plugin arguments.

--- src/Language/HERMIT/Pass.hs
+++ src/Language/HERMIT/Pass.hs
@@ -5,20 +5,34 @@ module Language.HERMIT.Pass (hermitPass, ppProgram, writeProgram) where
 import GhcPlugins
 import PprCore -- compiler/coreSyn/PprCore.lhs
 
+import Data.List
 import Control.Monad
 
 import Language.HERMIT.CommandLine as CommandLine
 
+
+-- Syntax:
+--   FullModuleName(filename),    <-- reads Module.hermit
+--   FullModuleName(-)            <-- starts the interpreter
+
 hermitPass :: [String] -> ModGuts -> CoreM ModGuts
 -- run the command-line option
-hermitPass ["cl"]  h      = do
-        liftIO $ print "command line!"
-        CommandLine.commandLine h
+hermitPass nms modGuts = case candidates of
+        [ ('/' : '-': []) ] -> do
+                CommandLine.commandLine modGuts
+        _ -> return modGuts
+   where
+           modName = showSDoc (ppr (mg_module modGuts))
+           candidates = [ drop (length modName) nm
+                        | nm <- nms
+                        , modName `isPrefixOf` nm
+                        ]
+{-        --
 -- find a function, interprete it (TODO)
 hermitPass ['@':nm]  h    = return h
 -- Need better error message here
 hermitPass other        h = error $ "hermitPass failed" ++ show other
-
+-}
 ppProgram :: ModGuts -> CoreM ModGuts
 ppProgram = bindsOnlyPass printBinds
 
