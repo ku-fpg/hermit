373fcb8b04debf462be6bd12708719869ce7fa6a
Andy Gill <andygill@ku.edu>
2012-04-08 19:41:21 -0500

    Adding better test (fib 45).

--- examples/first/Main.hs
+++ examples/first/Main.hs
@@ -7,16 +7,8 @@ module Main where
 
 import Language.HERMIT
 
-main = print (fib 40)
+main = print (fib 45)
 
 fib :: Int -> Int
 fib n = if n < 2 then 1 else fib(n-1) + fib (n-2)
 
-example :: Integer
-example = 99
-
-{-
-rewrite_fib :: H
-rewrite_fib =
-        consider 'fib []
--}
