7f314649bd65ee2fbf8581dfea2f6e9208bac5a6
Neil Sculthorpe <neil@ittc.ku.edu>
2012-05-11 11:18:48 -0500

    Restructured commandline interface.

--- src/Language/HERMIT/Command.hs
+++ src/Language/HERMIT/Command.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE KindSignatures, GADTs, TupleSections #-}
+{-# LANGUAGE KindSignatures, GADTs #-}
 
-module Language.HERMIT.Command
-        ( Command(..)
+module Language.HERMIT.Command ( 
+          
+          Command(..)
+        , KernelOutput(..)  
         , runCommands
-        ) where
+) where
 
 import GhcPlugins
 import Control.Monad
@@ -25,23 +27,32 @@ data Command :: * where
    SuperPopFocus ::                             Command
 
 instance Show Command where
-   show (Exit)          = "Exit"
-   show (Apply _)       = "Apply"
-   show (Query _)       = "Query"
-   show (PushFocus _)   = "PushFocus"
-   show (PopFocus)      = "PopFocus"
-   show (SuperPopFocus) = "SuperPopFocus"
-   show (Message _)     = "Message"
+   show Exit           = "Exit"
+   show (Apply _)      = "Apply"
+   show (Query _)      = "Query"
+   show (PushFocus _)  = "PushFocus"
+   show PopFocus       = "PopFocus"
+   show SuperPopFocus  = "SuperPopFocus"
+   show (Message _)    = "Message"
 
+-- | 'KernalOutput' is what the HERMIT kernel sends back.
+data KernelOutput :: * where
+   ErrorMsg    :: String            -> KernelOutput
+   QueryResult :: String            -> KernelOutput
+   FocusChange :: HermitEnv -> Core -> KernelOutput
+   CoreChange  :: Core              -> KernelOutput
 
-type Pop = (HermitEnv,(Core -> HermitM Core))
+instance Show KernelOutput where
+   show (ErrorMsg msg)    = "Error message: " ++ msg
+   show (QueryResult msg) = "Query result: " ++ msg
+   show (FocusChange _ _) = "Focus change."
+   show (CoreChange _)    = "Rewrite successfully applied."
 
-runCommands :: (HermitEnv -> Core -> IO Command)  -- waiting for commands
-            -> (String -> IO ())                  -- where to send output
-            -> ModGuts -> CoreM ModGuts
-runCommands getCommand output modGuts = do
-        ModGutsCore modGuts' <- loop [] c0 a0
-        return modGuts'
+type Pop = (HermitEnv, Core -> HermitM Core)
+
+runCommands :: CoreM Command -> (KernelOutput -> CoreM ()) -> ModGuts -> CoreM ModGuts
+runCommands getCommand output modGuts = do ModGutsCore modGuts' <- newFocus [] c0 a0
+                                           return modGuts'
   where
     c0 :: HermitEnv
     c0 = initHermitEnv
@@ -49,30 +60,40 @@ runCommands getCommand output modGuts = do
     a0 :: Core
     a0 = ModGutsCore modGuts
 
+    queryOut :: String -> CoreM ()
+    queryOut = output . QueryResult
+        
+    errOut :: String -> CoreM ()
+    errOut =  output . ErrorMsg
+
+    newFocus :: [Pop] -> HermitEnv -> Core -> CoreM Core
+    newFocus pops c a = (output $ FocusChange c a) >> loop pops c a
+
     loop :: [Pop] -> HermitEnv -> Core -> CoreM Core
-    loop pops c a = do rep <- liftIO (getCommand c a)
-                       case rep of
-                         PushFocus l   -> runHermitMR (\ ((c',b),k) -> loop ((c,k):pops) c' b) printAndLoop (apply l c a)
-                         PopFocus      -> popAndLoop pops
-                         SuperPopFocus -> popAll pops >>= loop [] c0
-                         Apply rr      -> runHermitMR (loop pops c) printAndLoop (apply rr c a)
-                         Query tt      -> runHermitMR printAndLoop printAndLoop (apply tt c a)
-                         Message msg   -> printAndLoop msg
+    loop pops c a = do cmd <- getCommand
+                       case cmd of
+                         Apply rr      -> runHermitMR coreChange errOutCont (apply rr c a)
+                         Query tt      -> runHermitMR queryOut errOut (apply tt c a) >> continue
+                         PushFocus l   -> runHermitMR (\ ((c',b),k) -> newFocus ((c,k):pops) c' b) errOutCont (apply l c a)
+                         PopFocus      -> case pops of
+                                            []           -> errOutCont "Nothing to pop, already at root."
+                                            ((c',k):cks) -> runHermitMR (newFocus cks c') errOutCont (k a)
+                         SuperPopFocus -> popAll pops >>= newFocus [] c0
                          Exit          -> popAll pops
+                         Message msg   -> continue -- currently messages are ignores
 
       where
-        popAndLoop :: [Pop] -> CoreM Core
-        popAndLoop []           = printAndLoop "Nothing to pop, already at root."
-        popAndLoop ((c',k):cks) = runHermitMR (loop cks c') printAndLoop (k a)
+        continue :: CoreM Core
+        continue = loop pops c a
+        
+        errOutCont :: String -> CoreM Core
+        errOutCont msg = errOut msg >> continue
 
+        coreChange :: Core -> CoreM Core
+        coreChange a' = output (CoreChange a') >> loop pops c a'
+        
         popAll :: [Pop] -> CoreM Core
         popAll []  = return a
         popAll cks = runHermitMR return
-                                 (\ msg -> printMsg msg >> return a0) -- if popping fails revert to initial value
+                                 ( \ msg -> errOut (msg ++ " Reverting to initial state.") >> return a0)
                                  (foldM (flip ($)) a (map snd cks))
-
-        printMsg :: String -> CoreM ()
-        printMsg = liftIO . output . show
-
-        printAndLoop :: String -> CoreM Core
-        printAndLoop s = printMsg s >> loop pops c a
--- src/Language/HERMIT/CommandLine.hs
+++ src/Language/HERMIT/CommandLine.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE RankNTypes, ScopedTypeVariables, FlexibleInstances, KindSignatures, GADTs, DataKinds, TypeOperators #-}
-
--- A Hermitage is a place of quiet reflection.
+{-# LANGUAGE FlexibleInstances, GADTs #-}
 
 module Language.HERMIT.CommandLine where
 
@@ -19,93 +17,54 @@ import qualified Language.HERMIT.Expr as Expr
 
 import Language.HERMIT.Primitive.Inline
 
-
-
 commandLine :: IO (Maybe String) -> ModGuts -> CoreM ModGuts
-commandLine gets modGuts = do
-    let getCmd :: HermitEnv -> Core -> IO Command
-        getCmd _ e = do
-          putStrLn (show2 e)
-          let loop = do
-                maybeLine <- gets
-                case maybeLine of
-                   Nothing -> return Exit
-                   Just line | all isSpace line -> loop
-                   Just ('-':'-': _) -> loop       -- comment
-                   Just line -> do
-                     case Expr.parseExpr line of
-                                 Left msg -> do
-                                     putStrLn $ "parse failure: " ++ show msg
-                                     loop
-                                 Right expr -> case interpExpr expr of
-                                                 Right cmd -> return cmd
-                                                 Left msg -> do
-                                                         putStrLn msg
-                                                         loop
-          loop
-
-    runCommands getCmd print modGuts
---    commands el 0 h
-
-{-
---- THIS CODE IS OLD
-commands :: forall c a . (Term a, Show2 a, Generic a ~ Core) => EditLine -> Int -> H.Hermitage c a -> CoreM (H.Hermitage c a)
-commands el n h = do
-         let (Context _ e) = H.getForeground h
---         liftIO $ putStrLn "Foreground: "
-         liftIO $ putStrLn (show2 e)
-         liftIO $ setPrompt el (return $ show n ++ "> ")
-         maybeLine <- liftIO $ elGets el
-         case maybeLine of
-             Nothing ->
-                do liftIO $ print "ctrl-D"
-                   return h
---             return h -- ctrl-D
-             Just line -> do
-                 let line' = init line -- remove trailing '\n'
-                 liftIO $ putStrLn $ "User input: " ++ show line'
-                 case words line' of
-                    ["?"] -> do
-                        let (Context _ e) = H.getForeground h
-                        liftIO $ putStrLn "Foreground: "
-                        liftIO $ putStrLn (show2 e)
-                        commands el n h
-                    [nstr] | all isDigit nstr ->
-                        focusCommand (focusOnPath [read nstr] :: Rewrite (Generic a) -> Rewrite a)
-                    ["*inline"] -> do
-                        res <- H.applyRewrite (extractR $ allbuR $ promoteR $ tryR $ inline) h
-                        case res of
-                          Left msg -> do
-                             liftIO $ print msg
-                             commands el n h
-                          Right h1 -> do
-                             commands el n h1
-                    ["focusB",nstr] | all isDigit nstr ->
-                        focusCommand (focusOnPath [read nstr] :: Rewrite (Bind Id) -> Rewrite a)
-                    ["focusE",nstr] | all isDigit nstr ->
-                        focusCommand (focusOnPath [read nstr] :: Rewrite (Expr Id) -> Rewrite a)
-                    other -> do
-                        liftIO $ putStrLn $ "do not understand " ++ show other
-                        commands el n h
+commandLine gets modGuts = runCommands (liftIO getCmd) (liftIO.printKernelOutput) modGuts
   where
-    focusCommand :: (Term b, Show2 b, Generic b ~ Core) => (Rewrite b -> Rewrite a) -> CoreM (H.Hermitage c a)
-    focusCommand kick =  do
-                        res <- H.focusHermitage kick h
-                        case res of
-                          Left msg -> do
-                             liftIO $ print msg
-                             commands el n h
-                          Right h1 -> do
-                             h2 <- commands el (succ n) h1
-                             res <- H.unfocusHermitage h2
-                             case res of
-                               Left msg -> do
-                                 liftIO $ print msg
-                                 commands el n h
-                               Right h3 -> do
-                                 commands el n h3
+    getCmd :: IO Command
+    getCmd = do maybeLine <- gets
+                case maybeLine of
+                  Nothing            -> return Exit
+                  Just ('-':'-':msg) -> return (Message msg)
+                  Just line          -> if all isSpace line 
+                                         then getCmd
+                                         else case Expr.parseExpr line of
+                                                Left  msg  -> putStrLn ("parse failure: " ++ msg) >> getCmd
+                                                Right expr -> case interpExpr expr of
+                                                                Left msg  -> putStrLn msg >> getCmd
+                                                                Right cmd -> return cmd
 
--}
+printKernelOutput :: KernelOutput -> IO ()
+printKernelOutput (ErrorMsg msg)    = putStrLn msg
+printKernelOutput (QueryResult msg) = putStrLn msg
+printKernelOutput (FocusChange _ a) = putStrLn (show2 a)
+printKernelOutput (CoreChange a)    = putStrLn (show2 a)
+
+-- THIS CODE IS FROM BEFORE THE KernalOutput DATA TYPE
+-- commandLine :: IO (Maybe String) -> ModGuts -> CoreM ModGuts
+-- commandLine gets modGuts = do
+--     let getCmd :: HermitEnv -> Core -> IO Command
+--         getCmd _ e = do
+--           putStrLn (show2 e)
+--           let loop = do
+--                 maybeLine <- gets
+--                 case maybeLine of
+--                    Nothing -> return Exit
+--                    Just line | all isSpace line -> loop
+--                    Just ('-':'-': _) -> loop       -- comment
+--                    Just line -> do
+--                      case Expr.parseExpr line of
+--                                  Left msg -> do
+--                                      putStrLn $ "parse failure: " ++ show msg
+--                                      loop
+--                                  Right expr -> case interpExpr expr of
+--                                                  Right cmd -> return cmd
+--                                                  Left msg -> do
+--                                                          putStrLn msg
+--                                                          loop
+--           loop
+
+--     runCommands (liftIO getCmd) print modGuts
+
 
 -- Later, this will have depth, and other pretty print options.
 class Show2 a where
@@ -113,7 +72,7 @@ class Show2 a where
 
 instance Show2 Core where
         show2 (ModGutsCore   m)  = show2 m
-        show2 (ProgramCore   p)  =  show2 p
+        show2 (ProgramCore   p)  = show2 p
         show2 (BindCore      bd) = show2 bd
         show2 (ExprCore      e)  = show2 e
         show2 (AltCore       a)  = show2 a
