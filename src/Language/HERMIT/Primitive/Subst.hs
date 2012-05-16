484da0d49c300a8b8b9ae500c012eed2d8b305c7
komp <komp@wifi-224.ittc.ku.edu>
2012-05-16 14:31:02 -0500

    improvements for subst.  Not yet complete.

--- src/Language/HERMIT/Primitive/Subst.hs
+++ src/Language/HERMIT/Primitive/Subst.hs
@@ -23,6 +23,8 @@ externals =
          [
            external "alpha" (promoteR alphaLambda)
                 [ "Alpha rename (for Lambda's)."]
+         , external "alpha-let" (promoteR alphaNonRecLet)
+                [ "Alpha rename (for Let)."]
          , external "let-sub" (promoteR letSubstR)
                 [ "Let substitution."]
          ]
@@ -32,37 +34,47 @@ bindList (NonRec b _) = [b]
 
 bindList (Rec binds) = map fst binds
 
+
+lamVar :: TranslateH CoreExpr Id
+lamVar = lamT (pure ()) const
+
+letNonRecVarAndType :: TranslateH CoreExpr (Id,Type)
+letNonRecVarAndType = letNonRecT idR (pure ()) (\ v e1 _ -> (v,exprType e1))
+
+letCaseVarAndType :: TranslateH CoreExpr (Id, Type)
+letCaseVarAndType = caseT idR (\ _ -> idR) (\ _ v ty _ -> (v, ty))
+
+-- returns (DomainType,ArgType)
+lamTypes :: TranslateH CoreExpr (Type,Type)
+lamTypes = liftMT (maybe (fail "Lambda argument is a TyVar.") pure . splitFunTy_maybe . exprType)
+                                         -- Not the best error message provided here.
+                                         -- Is this really (all) that can be happening here?
+
+varNameH :: Id -> TH.Name
+varNameH = TH.mkName . showSDoc . ppr
+
 -- The alpha renaming functions defined here,
 -- rely on a function to return a globally fresh Id,
 -- therefore, they do not require a list of Id's which must not clash.
 alphaLambda :: RewriteH CoreExpr
-alphaLambda = rewrite $ \ c exp ->
-          case exp of
-            (Lam b e) ->
-                let bNamestring = TH.mkName $ showSDoc (ppr b)
-                in
-                  case splitFunTy_maybe (exprType exp) of
-                    -- Not the best error message provided here.
-                    -- Is this really (all) that can be happening here?
-                    Nothing -> fail "Cannot rename. Lambda argument is a TyVar."
-                    Just (domain_ty, arg_ty) -> do
-                      freshId <-newVarH bNamestring domain_ty
-                      Lam freshId <$> apply (substR b (Var freshId)) (c @@ 0) e
-            _ -> fail "expr is not a Lambda"
+alphaLambda = do v <- lamVar
+                 (dom_ty, _) <- lamTypes
+                 v' <- constMT $ newVarH (varNameH v) dom_ty
+                 lamT (substR v $ Var v') (\ _ -> Lam v')
+
 
 -- Replace each var bound in a let expr with a globally fresh Id.
-alphaLet = rewrite $ \ c exp ->
-        case exp of
-            (Let (NonRec b be) e) ->
-                let bNamestring = TH.mkName $ showSDoc (ppr b)
-                in
-                  case splitFunTy_maybe (exprType exp) of
-                    Nothing -> fail "Cannot rename. Lambda argument is a TyVar."
-                    Just (domain_ty, arg_ty) -> do
-                      freshId <-newVarH bNamestring domain_ty
-                      Lam freshId <$> apply (substR b (Var freshId)) (c @@ 0) e
-            (Let (Rec binds) e) -> fail "Have not yet implemented alpha renaming for recursive let"
-            _ -> fail "expr is not a Lambda"
+
+alphaNonRecLet :: RewriteH CoreExpr
+alphaNonRecLet = do (v,ty) <- letNonRecVarAndType
+                    v' <- constMT $ newVarH (varNameH v) ty
+                    letNonRecT idR (substR v $ Var v') (\ _ e1 e2 -> Let (NonRec v' e1) e2)
+
+alphaRecLet :: RewriteH CoreExpr
+alphaRecLet = fail "Unable to perform alpha renaming for recursiveLet."
+
+alphaLet :: RewriteH CoreExpr
+alphaLet = alphaNonRecLet >-> alphaRecLet
 
 letSubstR :: RewriteH CoreExpr
 letSubstR = rewrite $ \ c exp ->
@@ -81,7 +93,7 @@ substRG :: Id -> CoreExpr  -> RewriteH Core
 substRG v e = promoteR $ substR v e
 
 substR :: Id -> CoreExpr  -> RewriteH CoreExpr
-substR v expReplacement = (rule12 <+ rule345 <+ rule78 {- <+ rule9 -} )  <+ rule6
+substR v expReplacement = (rule12 <+ rule345 <+ rule78 <+ rule9)  <+ rule6
     where -- The 6 rules from the textbook for the simple lambda calculus.
         rule12 :: RewriteH CoreExpr
         rule12 = rewrite $ \ c exp ->
@@ -94,10 +106,10 @@ substR v expReplacement = (rule12 <+ rule345 <+ rule78 {- <+ rule9 -} )  <+ rule
                   case exp of
                     Lam b e | (b == v) -> return exp
                     Lam b e | (b `notElem` freeIds expReplacement) ->
-                                Lam b <$> apply (allR (substRG v expReplacement))  (c @@ 0) e
+                                apply (lamT (substR v expReplacement) Lam) c exp
                     Lam b e | (v `notElem` freeIds e) -> return exp
                     Lam b e ->
-                        Lam b <$> apply (alphaLambda  >-> substR v expReplacement) (c @@ 0) e
+                        apply (alphaLambda  >-> lamT (substR v expReplacement) Lam) c exp
                     _ -> fail $ "not a Lambda"
 
         rule6 = allR (substRG v expReplacement)
@@ -106,32 +118,33 @@ substR v expReplacement = (rule12 <+ rule345 <+ rule78 {- <+ rule9 -} )  <+ rule
         rule78 = rewrite $ \ c exp ->
                  case exp of
                    Let bds e | (v `elem` (bindList bds)) -> return exp
-                   -- Don't we need to perform the subst over the let-bound expr's ???
-                   -- edk It appears that we need to effectively replicate code from allR for Let here ???
                    Let bds e | (null $ List.intersect (bindList bds) (freeIds expReplacement)) ->
-                                 Let <$> apply (allR (substRG v expReplacement)) (c @@ 0) bds
-                                         <*>  apply (allR (substRG v expReplacement)) (c @@ 1) e
+                                 apply (letT (substBindR v expReplacement)  (substR v expReplacement)  Let) c exp
                    -- If v is not free in e, it may be free in the expression(s) bound by the let
                    Let bds e | (v `notElem` freeIds e) ->
-                                 do bds' <- apply (allR (substRG v expReplacement)) (c @@ 0) bds
-                                    return (Let bds' e)
+                                 apply (letT (substBindR v expReplacement) idR Let) c exp
                    -- final case.  v is free in e, but the bound var(s) in the let appear
-                   -- edk free in the replacement expression.  Alpha renaming to the rescue, and try again.
-                   Let _ _ -> apply (alphaLet  >-> (substR v expReplacement)) c exp
+                   -- free in the replacement expression.  Alpha renaming to the rescue, and try again.
+                   Let _ _ -> apply (alphaLet  >-> (letT idR (substR v expReplacement) Let)) c exp
                    _ -> fail $ "not a Let"
+        -- edk?  Do we need to worry about clashes with the VBind component of a Case?
+        --  For now, it is ignored here.
+        rule9 = caseT (substR v expReplacement) (\_ -> substAltR v expReplacement) Case
 
-        rule9 = subst4Case v expReplacement
 
--- cases are a bit different, since v may be free in some branches but not in others
-subst4Case :: Id -> CoreExpr -> RewriteH CoreExpr
-subst4Case v expReplacement = rewrite $ \ c exp ->
-          case exp of
-            (Case e b ty alts) -> do
-                     e' <- apply (substR v expReplacement) (c @@ 0) e
-                     -- edk.  If 'b' is free in expReplacement, there is more work to do
-                     alts' <- sequence [apply (subst4Alt v expReplacement) (c @@ n) alt
-                                           | (alt,n) <- zip alts [1..] ]
-                     return $ Case e' b ty alts'
+substAltR :: Id -> CoreExpr -> RewriteH CoreAlt
+substAltR v expReplacement = rewrite $ \ c exp ->
+      case exp of
+        (con, bs, e) | v `elem` bs -> return exp
+                     | (null $ List.intersect bs (freeIds expReplacement)) ->
+                         apply (altT (substR v expReplacement)  (,,)) c exp
+                     | otherwise -> fail "Do not handle clashes with Case bound variables yet."
 
-subst4Alt :: Id -> CoreExpr -> RewriteH CoreAlt
-subst4Alt v expReplacement = undefined
+
+substBindR :: Id -> CoreExpr  -> RewriteH CoreBind
+substBindR v expReplacement = rewrite $ \ c exp ->
+       case exp of
+         (NonRec b e) | b == v -> return exp
+                      | (b `notElem` freeIds expReplacement) -> apply (nonRecT (substR v expReplacement) NonRec) c exp
+                      | otherwise -> return exp
+         _ -> fail "Do not handle recursive lets yet."
