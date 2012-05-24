b70b0f6d6b8de1b9df49d54b9cde0437ae25edeb
Neil Sculthorpe <neil@ittc.ku.edu>
2012-05-24 15:26:17 -0500

    Added deshadowing.

--- src/Language/HERMIT/Primitive/GHC.hs
+++ src/Language/HERMIT/Primitive/GHC.hs
@@ -2,25 +2,26 @@ module Language.HERMIT.Primitive.GHC where
 
 import GhcPlugins
 
-import Data.List (nub)
 import Control.Applicative
 
 import Language.KURE
 
 import Language.HERMIT.HermitKure
-import Language.HERMIT.HermitEnv
-import Language.HERMIT.HermitMonad
 import Language.HERMIT.External
 
-import qualified Language.Haskell.TH as TH
+------------------------------------------------------------------------
 
 externals :: [External]
 externals = map (.+ GHC)
          [ external "let-subst" (promoteR letSubstR)
                 [ "Let substitution [via GHC]"]
          , external "freevars" (promoteT freeIdsQuery)
-                [ "List the free variables in this expression." ]
+                [ "List the free variables in this expression [via GHC]" ]
+         , external "deshadow-binds" (promoteR deShadowBindsR)
+                [ "Deshadow a program [via GHC]" ]
          ]
+
+------------------------------------------------------------------------
 
 letSubstR :: RewriteH CoreExpr
 letSubstR = rewrite $ \ c exp -> case exp of
@@ -34,6 +35,7 @@ letSubstR = rewrite $ \ c exp -> case exp of
                 return $ substExpr (text "letSubstR") sub e
       _ -> fail "LetSubst failed. Expr is not a (non-recursive) Let."
 
+------------------------------------------------------------------------
 
 -- output a list of all free variables in the Expr.
 freeIdsQuery :: TranslateH CoreExpr String
@@ -45,3 +47,16 @@ freeIdsT = liftT freeIds
 -- note: exprFreeIds is only value-level free variables
 freeIds :: CoreExpr -> [Id]
 freeIds  = uniqSetToList . exprFreeVars
+
+------------------------------------------------------------------------
+
+-- | [from GHC documentation] De-shadowing the program is sometimes a useful pre-pass.
+-- It can be done simply by running over the bindings with an empty substitution,
+-- becuase substitution returns a result that has no-shadowing guaranteed.
+--
+-- (Actually, within a single /type/ there might still be shadowing, because
+-- 'substTy' is a no-op for the empty substitution, but that's probably OK.)
+deShadowBindsR :: RewriteH CoreProgram
+deShadowBindsR = liftT deShadowBinds
+
+------------------------------------------------------------------------
