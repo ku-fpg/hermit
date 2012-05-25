4a23cc5ccfbde4311fa9fe4cb9e51092e58aba08
Andy Gill <andygill@ku.edu>
2012-05-25 11:00:14 -0500

    Adding test input to the reverse.

--- tests/prims/Reverse.hs
+++ tests/prims/Reverse.hs
@@ -1,6 +1,20 @@
+module Main where
+
 import Prelude hiding (reverse)
 
 main = print (reverse "Hello, World")
 
 reverse []     = []
 reverse (x:xs) = reverse xs ++ [x]
+
+foo :: Int -> Bool -> Float -> Int
+foo a b c = f (x + y + z)
+  where
+     {-# NOINLINE x #-}
+     x = 1
+     {-# NOINLINE y #-}
+     y = 2
+     {-# NOINLINE z #-}
+     z = 3 + a
+     {-# NOINLINE f #-}
+     f x = x + 1
