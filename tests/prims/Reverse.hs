2a752476d93b21c67b270254c7b92533d3bdd616
Andy Gill <andygill@ku.edu>
2012-05-26 11:41:52 -0500

    Adding RULE to reverse, again.

--- tests/prims/Reverse.hs
+++ tests/prims/Reverse.hs
@@ -2,10 +2,16 @@ module Main where
 
 import Prelude hiding (reverse)
 
+{-# RULES "append-assoc-left" forall xs ys zs . (xs ++ ys) ++ zs = xs ++ (ys ++ zs) #-}
+
 main = print (reverse "Hello, World")
 
 reverse []     = []
 reverse (x:xs) = reverse xs ++ [x]
+
+test :: [Int]
+test = ([1] ++ [2]) ++ [3]
+
 
 foo :: Int -> Bool -> Float -> Int
 foo a b c = f (x + y + z)
