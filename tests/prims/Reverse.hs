966008de67c0d542b044b1c67cceb22f1a7073d9
Andy Gill <andygill@ku.edu>
2012-05-29 22:47:28 -0500

    Best reverse script so far.

--- tests/prims/Reverse.hs
+++ tests/prims/Reverse.hs
@@ -1,20 +1,6 @@
 module Main where
 
-import Data.Function (fix)
-
-type H a = [a] -> [a]
-
-repH :: [a] -> H a
-repH xs = (xs ++)
-
-absH :: H a -> [a]
-absH f = f []
-
-unwrap :: ([a] -> [a]) -> ([a] -> H a)
-unwrap f = repH . f
-
-wrap :: ([a] -> H a) -> ([a] -> [a])
-wrap g = absH . g
+import HERMIT
 
 {-# RULES "ww" forall work . fix work = wrap (fix (unwrap . work . wrap)) #-}
 
