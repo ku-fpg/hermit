fb9cbbae7f91f83e5d878a1ba41aa6522762d6e4
Andy Gill <andygill@ku.edu>
2012-06-13 15:44:33 -0500

    Changing quit into resume (GHC comp), and abort (GHC comp).

--- src/Language/HERMIT/Kernel.hs
+++ src/Language/HERMIT/Kernel.hs
@@ -1,44 +1,29 @@
 {-# LANGUAGE KindSignatures, GADTs, RankNTypes, ScopedTypeVariables, TypeFamilies, DeriveDataTypeable #-}
 
-module Language.HERMIT.Kernel (
-
-          KernelCommand(..)
---        , KernelOutput(..)
---        , runCommands
-        , interpKernelCommand
-        , hermitKernel
+module Language.HERMIT.Kernel
+        (hermitKernel
         , Kernel(..)
         , AST(..)
-        , KernelCommandBox(..)
-
-) where
+        ) where
 
 import GhcPlugins
-import Data.Dynamic
 
 import Language.HERMIT.HermitKure
 import Language.HERMIT.HermitEnv
 import Language.HERMIT.HermitMonad
-import Language.HERMIT.Interp
-import Language.HERMIT.External
+import Language.HERMIT.GHC
 
 import qualified Data.Map as M
 import Control.Concurrent
 
 data Kernel = Kernel
-        { quitK   ::            AST                      -> IO ()
+        { resumeK ::            AST                      -> IO ()
+        , abortK  ::                                        IO ()
         , applyK  ::            AST -> RewriteH Core     -> IO AST
         , queryK  :: forall a . AST -> TranslateH Core a -> IO a
         , deleteK ::            AST                      -> IO ()
         , listK   ::                                        IO [AST]
         }
-
-interpKernelCommand :: [Interp KernelCommand]
-interpKernelCommand =
-             [ Interp $ \ (KernelCommandBox cmd)      -> cmd
-             , Interp $ \ (RewriteCoreBox rr)         -> Apply rr
-             , Interp $ \ (TranslateCoreStringBox tt) -> Query tt
-             ]
 
 -- a name of a syntax tree
 newtype AST = AST Int
@@ -66,7 +51,8 @@ hermitKernel callback modGuts = do
                         loop (succ n)
                  in loop 0
 
-        let sendDone = putMVar msg . Done
+        let sendDone :: (M.Map AST ModGuts -> CoreM ModGuts) -> IO ()
+            sendDone = putMVar msg . Done
 
         let sendReq :: (M.Map AST ModGuts -> CoreM (Either String (a,M.Map AST ModGuts))) -> IO a
             sendReq fn = do
@@ -87,7 +73,8 @@ hermitKernel callback modGuts = do
         let fail' m = return (Left m)
 
         let kernel = Kernel
-                { quitK = \ name -> sendDone $ \ env -> find name env fail $ \ core -> return core
+                { resumeK = \ name -> sendDone $ \ env -> find name env fail $ \ core -> return core
+                , abortK  = sendDone $ \ _ -> throwGhcException (ProgramError "<HERMIT>: hard GHC abort requested")
                 , applyK = \ name rr -> sendReq $ \ env -> find name env fail' $ \ core ->
                              runHermitMR
                                   (\ core' -> do
@@ -130,99 +117,3 @@ hermitKernel callback modGuts = do
 
         return modGuts'
 
-
--- | 'KernelCommand' is what you send to the HERMIT kernel.
-data KernelCommand :: * where
-   Exit          ::                             KernelCommand
---   Status        ::                             KernelCommand
---   Message       :: String                   -> KernelCommand
-   Apply         :: RewriteH Core            -> KernelCommand
-   Query          :: TranslateH Core String   -> KernelCommand  -- strange stuff
---   PushFocus     :: LensH Core Core          -> KernelCommand
---   PopFocus      ::                             KernelCommand
---   SuperPopFocus ::                             KernelCommand
-
-
-data KernelCommandBox = KernelCommandBox KernelCommand deriving Typeable
-
-instance Extern KernelCommand where
-    type Box KernelCommand = KernelCommandBox
-    box i = KernelCommandBox i
-    unbox (KernelCommandBox i) = i
-
-
-instance Show KernelCommand where
-   show Exit           = "Exit"
----   show Status         = "Status"
-   show (Apply _)      = "Apply"
-   show (Query _)      = "Query"
---   show (PushFocus _)  = "PushFocus"
---   show PopFocus       = "PopFocus"
---   show SuperPopFocus  = "SuperPopFocus"
---   show (Message msg)  = "Message: " ++ msg
-{-
--- | 'KernalOutput' is what the HERMIT kernel sends back.
-data KernelOutput :: * where
-   ErrorMsg    :: String            -> KernelOutput
-   QueryResult :: String            -> KernelOutput
-   FocusChange :: HermitEnv -> Core -> KernelOutput
-   CoreChange  :: Core              -> KernelOutput
-
-instance Show KernelOutput where
-   show (ErrorMsg msg)    = "Error message: " ++ msg
-   show (QueryResult msg) = "Query result: " ++ msg
-   show (FocusChange _ _) = "Focus change"
-   show (CoreChange _)    = "Core change"
-
-type Pop = (HermitEnv, Core -> HermitM Core)
-
-
-runCommands :: CoreM KernelCommand -> (KernelOutput -> CoreM ()) -> ModGuts -> CoreM ModGuts
-runCommands getCommand output modGuts = do ModGutsCore modGuts' <- newFocus [] c0 a0
-                                           return modGuts'
-  where
-    c0 :: HermitEnv
-    c0 = initHermitEnv
-
-    a0 :: Core
-    a0 = ModGutsCore modGuts
-
-    queryOut :: String -> CoreM ()
-    queryOut = output . QueryResult
-
-    errOut :: String -> CoreM ()
-    errOut =  output . ErrorMsg
-
-    newFocus :: [Pop] -> HermitEnv -> Core -> CoreM Core
-    newFocus pops c a = (output $ FocusChange c a) >> loop pops c a
-
-    loop :: [Pop] -> HermitEnv -> Core -> CoreM Core
-    loop pops c a = do cmd <- getCommand
-                       case cmd of
-                         Apply rr      -> runHermitMR coreChange errOutCont (apply rr c a)
-                         Query tt      -> runHermitMR queryOut errOut (apply tt c a) >> continue
-                         PushFocus l   -> runHermitMR (\ ((c',b),k) -> newFocus ((c,k):pops) c' b) errOutCont (apply l c a)
-                         PopFocus      -> case pops of
-                                            []           -> errOutCont "Nothing to pop, already at root."
-                                            ((c',k):cks) -> runHermitMR (newFocus cks c') errOutCont (k a)
-                         SuperPopFocus -> popAll pops >>= newFocus [] c0
-                         Exit          -> popAll pops
-                         Status        -> newFocus pops c a
-                         Message _     -> continue -- currently messages are ignored
-
-      where
-        continue :: CoreM Core
-        continue = loop pops c a
-
-        errOutCont :: String -> CoreM Core
-        errOutCont msg = errOut msg >> continue
-
-        coreChange :: Core -> CoreM Core
-        coreChange a' = output (CoreChange a') >> loop pops c a'
-
-        popAll :: [Pop] -> CoreM Core
-        popAll []  = return a
-        popAll cks = runHermitMR return
-                                 ( \ msg -> errOut (msg ++ " Reverting to initial state.") >> return a0)
-                                 (foldM (flip ($)) a (map snd cks))
--}
