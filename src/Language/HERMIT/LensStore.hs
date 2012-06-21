6cda54a2a74134af8f7ce6b850e832ba50496fb5
Andrew Farmer <anfarmer@ku.edu>
2012-06-21 17:42:10 -0500

    Rewrite LensStore to give back new ZST on modPathL/beginScopeL/endScopeL

--- src/Language/HERMIT/LensStore.hs
+++ src/Language/HERMIT/LensStore.hs
@@ -15,21 +15,22 @@ data LensStore = LensStore
         , queryL      :: forall a . ZST -> TranslateH Core a -> IO a
         , deleteL     ::            ZST                      -> IO ()
         , listL       ::                                        IO [ZST]
-        , modPathL    ::            ZST -> (Path -> Path)    -> IO ()
-        , scopeStartL ::            ZST                      -> IO ()
-        , scopeEndL   ::            ZST                      -> IO ()
+        , modPathL    ::            ZST -> (Path -> Path)    -> IO ZST
+        , beginScopeL ::            ZST                      -> IO ZST
+        , endScopeL   ::            ZST                      -> IO ZST
         }
 
 data ZST = ZST Int
     deriving (Eq, Ord, Show)
 
-type ZSTStore = I.IntMap (AST, [Path])
+-- path stack, representing the base path, then the relative path
+type ZSTStore = I.IntMap (AST, [Path], Path)
 
-get :: Monad m => Int -> ZSTStore -> m (AST, [Path])
+get :: Monad m => Int -> ZSTStore -> m (AST, [Path], Path)
 get zst m = maybe (fail "lensStore: invalid ZST") return (I.lookup zst m)
 
-pathStackToLens :: [Path] -> LensH Core Core
-pathStackToLens = pathL . concat . reverse
+pathStackToLens :: [Path] -> Path -> LensH Core Core
+pathStackToLens base = pathL . (concat (reverse base) ++)
 
 -- not sure about this interface
 -- hermitKernel :: (Kernel -> AST -> IO ()) -> ModGuts -> CoreM ModGuts
@@ -37,7 +38,7 @@ pathStackToLens = pathL . concat . reverse
 -- hermitKernel $ lensStore $ \ lenstore zst -> do ...
 lensStore :: (LensStore -> ZST -> IO ()) -> Kernel -> AST -> IO ()
 lensStore callback kernel initAST = do
-    store <- newTMVarIO $ I.fromList [(0,(initAST, [[]]))]
+    store <- newTMVarIO $ I.fromList [(0,(initAST, [], []))]
     key <- newTMVarIO (1::Int)
 
     let newKey = do
@@ -46,47 +47,53 @@ lensStore callback kernel initAST = do
             return k
 
         ls = LensStore
-            { resumeL = \ (ZST zst) -> do
-                            m <- atomically $ readTMVar store
-                            (ast,_) <- get zst m
-                            resumeK kernel ast
-            , abortL = abortK kernel
-            , applyL = \ (ZST zst) rr -> do
-                            m <- atomically $ takeTMVar store
-                            (ast, ps) <- get zst m
-                            ast' <- applyK kernel ast (focusR (pathStackToLens ps) rr)
-                            atomically $ do
+            { resumeL     = \ (ZST zst) -> do
+                                m <- atomically $ readTMVar store
+                                (ast,_,_) <- get zst m
+                                resumeK kernel ast
+            , abortL      = abortK kernel
+            , applyL      = \ (ZST zst) rr -> do
+                                m <- atomically $ takeTMVar store
+                                (ast, base, rel) <- get zst m
+                                ast' <- applyK kernel ast (focusR (pathStackToLens base rel) rr)
+                                atomically $ do
+                                    k <- newKey
+                                    putTMVar store $ I.insert k (ast', base, rel) m
+                                    return $ ZST k
+            , queryL      = \ (ZST zst) t -> do
+                                m <- atomically $ readTMVar store
+                                (ast, base, rel) <- get zst m
+                                queryK kernel ast (focusT (pathStackToLens base rel) t)
+            , deleteL     = \ (ZST zst) -> atomically $ do
+                                m <- takeTMVar store
+                                putTMVar store $ I.delete zst m
+            , listL       = do m <- atomically $ readTMVar store
+                               return [ ZST zst | zst <- I.keys m ]
+            , modPathL    = \ (ZST zst) f -> do
+                                m <- atomically $ takeTMVar store
+                                (ast, base, rel) <- get zst m
+                                let rel' = f rel
+                                condM (fmap (&& (rel /= rel')) (queryK kernel ast (testLensT (pathStackToLens base rel'))))
+                                      (atomically $ do
                                             k <- newKey
-                                            putTMVar store $ I.insert k (ast', ps) m
-                                            return (ZST k)
-            , queryL = \ (ZST zst) t -> do
-                                        m <- atomically $ readTMVar store
-                                        (ast, ps) <- get zst m
-                                        queryK kernel ast (focusT (pathStackToLens ps) t)
-            , deleteL = \ (ZST zst) -> atomically $ do
-                                        m <- takeTMVar store
-                                        putTMVar store $ I.delete zst m
-            , listL = do m <- atomically $ readTMVar store
-                         return [ ZST zst | zst <- I.keys m ]
-            , modPathL = \ (ZST zst) f -> do
-                                        m <- atomically $ takeTMVar store
-                                        (ast, p:ps) <- get zst m
-                                        let p' = f p
-                                        if p == p'
-                                        then atomically $ putTMVar store m
-                                        else do condM (queryK kernel ast (testLensT (pathStackToLens (p':ps))))
-                                                      (atomically $ putTMVar store $ I.insert zst (ast, p':ps) m)
-                                                      (atomically $ putTMVar store m) -- throw error too?
-            , scopeStartL = \ (ZST zst) -> atomically $ do
-                                        m <- takeTMVar store
-                                        (ast, ps) <- get zst m
-                                        putTMVar store $ I.insert zst (ast, []:ps) m
-            , scopeEndL = \ (ZST zst) -> atomically $ do
-                                        m <- takeTMVar store
-                                        (ast, _:ps) <- get zst m
-                                        case ps of
-                                            [] -> fail "lensStore: no scope to end"
-                                            _  -> putTMVar store $ I.insert zst (ast, ps) m
+                                            putTMVar store $ I.insert k (ast, base, rel') m
+                                            return $ ZST k)
+                                      (atomically $ putTMVar store m >> return (ZST zst)) -- throw error too?
+            , beginScopeL = \ (ZST zst) -> atomically $ do
+                                m <- takeTMVar store
+                                (ast, base, rel) <- get zst m
+                                k <- newKey
+                                putTMVar store $ I.insert k (ast, rel : base, []) m
+                                return $ ZST k
+            , endScopeL   = \ (ZST zst) -> atomically $ do
+                                m <- takeTMVar store
+                                (ast, base, _) <- get zst m
+                                case base of
+                                    [] -> fail "lensStore: no scope to end"
+                                    (rel:base') -> do
+                                        k <- newKey
+                                        putTMVar store $ I.insert k (ast, base', rel) m
+                                        return $ ZST k
             }
 
     callback ls $ ZST 0
