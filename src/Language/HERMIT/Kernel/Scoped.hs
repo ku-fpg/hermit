e2f2cef1d9a56079d433de8384f70045220a1395
Andrew Farmer <anfarmer@ku.edu>
2012-06-22 14:57:19 -0500

    Rename LensStore to ScopedKernel, add pathS :: SAST -> IO [Path]

---
+++ src/Language/HERMIT/Kernel/Scoped.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE RankNTypes #-}
+module Language.HERMIT.Kernel.Scoped (ScopedKernel(..), SAST(..), scopedKernel) where
+
+import Control.Concurrent.STM
+
+import qualified Data.IntMap as I
+
+import GhcPlugins
+
+import Language.HERMIT.HermitKure
+import Language.HERMIT.Kernel
+
+data ScopedKernel = ScopedKernel
+        { resumeS     ::            SAST                      -> IO ()
+        , abortS      ::                                        IO ()
+        , applyS      ::            SAST -> RewriteH Core     -> IO SAST
+        , queryS      :: forall a . SAST -> TranslateH Core a -> IO a
+        , deleteS     ::            SAST                      -> IO ()
+        , listS       ::                                        IO [SAST]
+        , pathS       ::            SAST                      -> IO [Path]
+        , modPathS    ::            SAST -> (Path -> Path)    -> IO SAST
+        , beginScopeS ::            SAST                      -> IO SAST
+        , endScopeS   ::            SAST                      -> IO SAST
+        }
+
+data SAST = SAST Int
+    deriving (Eq, Ord, Show)
+
+-- path stack, representing the base path, then the relative path
+type SASTStore = I.IntMap (AST, [Path], Path)
+
+get :: Monad m => Int -> SASTStore -> m (AST, [Path], Path)
+get sAst m = maybe (fail "scopedKernel: invalid SAST") return (I.lookup sAst m)
+
+pathStackToLens :: [Path] -> Path -> LensH Core Core
+pathStackToLens base = pathL . (concat (reverse base) ++)
+
+scopedKernel :: (ScopedKernel -> SAST -> IO ()) -> ModGuts -> CoreM ModGuts
+scopedKernel callback = hermitKernel $ \ kernel initAST -> do
+    store <- newTMVarIO $ I.fromList [(0,(initAST, [], []))]
+    key <- newTMVarIO (1::Int)
+
+    let newKey = do
+            k <- takeTMVar key
+            putTMVar key (k+1)
+            return k
+
+        skernel = ScopedKernel
+            { resumeS     = \ (SAST sAst) -> do
+                                m <- atomically $ readTMVar store
+                                (ast,_,_) <- get sAst m
+                                resumeK kernel ast
+            , abortS      = abortK kernel
+            , applyS      = \ (SAST sAst) rr -> do
+                                m <- atomically $ takeTMVar store
+                                (ast, base, rel) <- get sAst m
+                                ast' <- applyK kernel ast (focusR (pathStackToLens base rel) rr)
+                                atomically $ do
+                                    k <- newKey
+                                    putTMVar store $ I.insert k (ast', base, rel) m
+                                    return $ SAST k
+            , queryS      = \ (SAST sAst) t -> do
+                                m <- atomically $ readTMVar store
+                                (ast, base, rel) <- get sAst m
+                                queryK kernel ast (focusT (pathStackToLens base rel) t)
+            , deleteS     = \ (SAST sAst) -> atomically $ do
+                                m <- takeTMVar store
+                                putTMVar store $ I.delete sAst m
+            , listS       = do m <- atomically $ readTMVar store
+                               return [ SAST sAst | sAst <- I.keys m ]
+            , pathS       = \ (SAST sAst) -> atomically $ do
+                                m <- readTMVar store
+                                (_, base, rel) <- get sAst m
+                                return $ reverse $ rel : base
+            , modPathS    = \ (SAST sAst) f -> do
+                                m <- atomically $ takeTMVar store
+                                (ast, base, rel) <- get sAst m
+                                let rel' = f rel
+                                condM (fmap (&& (rel /= rel')) (queryK kernel ast (testLensT (pathStackToLens base rel'))))
+                                      (atomically $ do
+                                            k <- newKey
+                                            putTMVar store $ I.insert k (ast, base, rel') m
+                                            return $ SAST k)
+                                      (atomically $ putTMVar store m >> return (SAST sAst)) -- throw error too?
+            , beginScopeS = \ (SAST sAst) -> atomically $ do
+                                m <- takeTMVar store
+                                (ast, base, rel) <- get sAst m
+                                k <- newKey
+                                putTMVar store $ I.insert k (ast, rel : base, []) m
+                                return $ SAST k
+            , endScopeS   = \ (SAST sAst) -> atomically $ do
+                                m <- takeTMVar store
+                                (ast, base, _) <- get sAst m
+                                case base of
+                                    [] -> fail "scopedKernel: no scope to end" -- will this fail undo the takeTMVar?
+                                    (rel:base') -> do
+                                        k <- newKey
+                                        putTMVar store $ I.insert k (ast, base', rel) m
+                                        return $ SAST k
+            }
+
+    callback skernel $ SAST 0
