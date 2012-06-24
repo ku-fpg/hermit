afe61ca08a5853547c48f84bf13c04e2e56dd4ba
Andy Gill <andygill@ku.edu>
2012-06-23 23:10:33 -0500

    Adding observe and observe-failure

--- src/Language/HERMIT/Primitive/Debug.hs
+++ src/Language/HERMIT/Primitive/Debug.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, TypeFamilies #-}
 module Language.HERMIT.Primitive.Debug where
 
 import GhcPlugins as GHC
@@ -11,14 +11,17 @@ externals :: [External]
 externals =
          [ external "trace" (traceR :: String -> RewriteH Core)
                 [ "give a side-effect message as output when processing this command" ]
---         , external "observe" (observeR :: String -> RewriteH Core)
---                [ "give a side-effect message as output, and observe the value being processed" ]
+         , external "observe" (observeR :: String -> RewriteH Core)
+                [ "give a side-effect message as output, and observe the value being processed" ]
+         , external "observe-failure" (observeFailureR :: String -> RewriteH Core -> RewriteH Core)
+                [ "give a side-effect message if the rewrite fails, including the failing input" ]
          ]
 
-
+observeFailureR :: (Injection a Core, Generic a ~ Core) => String -> RewriteH a -> RewriteH a
+observeFailureR str m = m <+ observeR str
 
 -- Print out the Core, with a message
-observeR :: (Injection a Core) => String -> RewriteH a
+observeR :: (Injection a Core, Generic a ~ Core) => String -> RewriteH a
 observeR msg = extractR $ sideEffectR $ \ _ core -> do
         liftIO (putStrLn $ "[traceR: " ++ msg ++ "]")
         liftIO (putStrLn $ show2 (core :: Core))
