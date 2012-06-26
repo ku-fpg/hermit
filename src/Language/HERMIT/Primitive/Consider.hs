336e279f08cfbe06f9505ed6d37ebfb0543424e1
Andy Gill <andygill@ku.edu>
2012-06-25 21:21:42 -0500

    Adding a rhs-of combinator, like consider.

--- src/Language/HERMIT/Primitive/Consider.hs
+++ src/Language/HERMIT/Primitive/Consider.hs
@@ -6,6 +6,8 @@ import Language.HERMIT.HermitKure
 import Language.HERMIT.External
 import Language.HERMIT.GHC
 
+import Control.Arrow
+
 import qualified Language.Haskell.TH as TH
 
 externals :: [External]
@@ -13,6 +15,8 @@ externals = map (.+ Lens)
             [
               external "consider" consider
                 [ "'consider <v>' focuses into a named binding <v>" ]
+            , external "rhs-of" rhsOf
+                [ "rhs-of 'name focuses into the right-hand-side of binding <v>" ]
         -- This is in the wrong place
             , external "var" (promoteR . var :: TH.Name -> RewriteH Core)
                 [ "var <v> succeeded for variable v, and fails otherwise"]
@@ -20,12 +24,23 @@ externals = map (.+ Lens)
 
 -- Focus on a bindings
 consider :: TH.Name -> TranslateH Core Path
-consider = uniquePrunePathToT . nameBound
+consider = uniquePrunePathToT . bindGroup
 
-nameBound :: TH.Name -> Core -> Bool
-nameBound nm (BindCore (NonRec v _))  =  nm `cmpName` idName v
-nameBound nm (BindCore (Rec bds))     =  any (cmpName nm . idName) $ map fst bds
-nameBound _  _                        =  False
+-- find a bind group that defineds a given name
+
+bindGroup :: TH.Name -> Core -> Bool
+bindGroup nm (BindCore (NonRec v _))  =  nm `cmpName` idName v
+bindGroup nm (BindCore (Rec bds))     =  any (cmpName nm . idName) $ map fst bds
+bindGroup _  _                        =  False
+
+-- find a specific binding's rhs.
+rhsOf :: TH.Name -> TranslateH Core Path
+rhsOf nm = uniquePrunePathToT (namedBinding nm) >>> arr (0 :)
+
+namedBinding :: TH.Name -> Core -> Bool
+namedBinding nm (BindCore (NonRec v _))  =  nm `cmpName` idName v
+namedBinding nm (DefCore (Def v _))      =  nm `cmpName` idName v
+namedBinding _  _                        =  False
 
 -- Hacks till we can find the correct way of doing these.
 cmpName :: TH.Name -> Name -> Bool
