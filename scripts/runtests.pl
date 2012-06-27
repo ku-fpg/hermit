63fa7bee9e9d147649339c15f6e0e9ced1102f14
Andrew Farmer <anfarmer@ku.edu>
2012-06-27 15:45:56 -0500

    Add case-reduce test. Add runtests.pl which runs ghc with -v0, so diff is cleaner (doesn't include package loading output, as we all have slightly different package versions). Add interactive option to Makefile for writing tests.

---
+++ scripts/runtests.pl
@@ -0,0 +1,27 @@
+#
+# Assuming that the HERMIT package has already been install
+#
+
+sub compile {
+	my ($mod,@opts) = @_;
+	print "compiling $mod, using ($mod,@opts)\n";
+
+	$str = "ghc-7.4.1 ${mod} " .
+               "    -fforce-recomp -O2 -dcore-lint -fsimple-list-literals -v0 ";
+
+	$str .= "-fplugin=HERMIT ";
+
+	push(@opts,"");  # This is so we have at least one main:Main
+	foreach (@opts) {
+	    $str .= "-fplugin-opt=HERMIT:main:Main:$_ ";
+	}
+	print "$str\n";
+
+	system($str);
+	exit($?);
+}
+
+compile(@ARGV);
+
+
+
