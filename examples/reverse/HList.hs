30867a3967bc312b63202638b6bc0fbce81e5d97
Andy Gill <andygill@ku.edu>
2012-07-07 16:17:00 -0500

    wobble to reverse example.

--- examples/reverse/HList.hs
+++ examples/reverse/HList.hs
@@ -19,8 +19,8 @@ absH f = f []
 {-# RULES "absH" forall f 	 . absH f = f []     #-}
 
 -- The "Algebra" for repH
-{-# RULES "repH ++" forall xs ys . repH (xs ++ ys) = repH xs . repH ys #-}
-{-# RULES "repH []" 	           repH [] = ([] ++) 	       	       #-}
+{-# RULES "repH ++" forall xs ys   . repH (xs ++ ys) = repH xs . repH ys #-}
+{-# RULES "repH []" 	             repH [] = id  	       	         #-}
 {-# RULES "repH (:)" forall x xs   . repH (x:xs) = ((:) x) . repH xs     #-}
 
 -- Should be in the "List" module
