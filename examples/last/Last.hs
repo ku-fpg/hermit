c61bf5cd65b8085070c8ee60961994d3e839cb87
Neil Sculthorpe <neil@ittc.ku.edu>
2012-07-15 12:09:03 -0500

    Checking in Last example (forgot to add before).

---
+++ examples/last/Last.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Data.Function(fix)
+import Prelude hiding (last)
+import LastPrelude
+
+unwrap         :: ([a] -> a) -> a -> [a] -> a
+unwrap f a as  = f (a:as)
+
+wrap           :: (a -> [a] -> a) -> [a] -> a
+wrap f []      = error "wrap _ []"
+wrap f (a:as)  = f a as
+
+
+{-# RULES "ww" forall f . fix f = wrap (fix (unwrap . f . wrap)) #-}
+{-# RULES "fix-computation" forall f . fix f = f (fix f) #-}
+
+-- last :: [a] -> a
+last []       = error "last []"
+last [a]      = a
+last (_:a:as) = last (a:as)
+
+main :: IO ()
+main = print (last "hello")
