dc0212420d58c3e89e2d9522110ab8059936952d
Neil Sculthorpe <neil@ittc.ku.edu>
2012-08-06 21:36:08 -0500

    Finished generic concat vanishes script.

--- examples/concatVanishes/Rev.hs
+++ examples/concatVanishes/Rev.hs
@@ -12,11 +12,10 @@ wrap :: ([a] -> H a) -> ([a] -> [a])
 wrap g = absH . g
 
 {-# RULES "ww" forall f . fix f = wrap (fix (unwrap . f . wrap)) #-}
-{-# RULES "inline-fix" forall f . fix f = let w = f w in w #-}
 
 -- rev :: [a] -> [a]
 rev []     = []
-rev (x:xs) = rev xs ++ [x]
+rev (y:ys) = rev ys ++ [y]
 
 main :: IO ()
 main = print $ length $ rev [1..15000]
