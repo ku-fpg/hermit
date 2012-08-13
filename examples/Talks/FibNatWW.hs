3fa0c675b863765070d155ef1d65d5eadb802dbf
Neil Sculthorpe <neil@ittc.ku.edu>
2012-08-13 10:33:46 -0500

    Added Fib script to Talks.

--- examples/Talks/FibNatWW.hs
+++ examples/Talks/FibNatWW.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Prelude hiding ((+))
+import GHC.Tuple
 import Data.Function (fix)
 import Nat
 
@@ -9,15 +10,14 @@ fib Zero             = Zero
 fib (Succ Zero)      = Succ Zero
 fib (Succ (Succ n))  = fib (Succ n) + fib n
 
-{-# INLINE wrap #-}
 wrap :: (Nat -> (Nat, Nat)) -> Nat -> Nat
 wrap h = fst . h
 
-{-# INLINE unwrap #-}
 unwrap :: (Nat -> Nat) -> Nat -> (Nat, Nat)
 unwrap h n = (h n, h (Succ n))
 
 {-# RULES "ww" forall f . fix f = wrap (fix (unwrap . f . wrap)) #-}
+{-# RULES "precondition" forall w . wrap (unwrap w) = w          #-}
 
 main :: IO ()
 main = print (fromNat $ fib $ toNat 30)
