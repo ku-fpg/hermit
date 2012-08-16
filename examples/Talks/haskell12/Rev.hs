70e0d50fd8958212511a6fbc19e293cd70c1ab18
Neil Sculthorpe <neil@ittc.ku.edu>
2012-08-16 15:40:23 -0500

    Reordering file.

--- examples/Talks/haskell12/Rev.hs
+++ examples/Talks/haskell12/Rev.hs
@@ -3,6 +3,10 @@ module Main where
 import HList
 import Data.Function (fix)
 
+-- rev :: [a] -> [a]
+rev []     = []
+rev (x:xs) = rev xs ++ [x]
+
 unwrap :: ([a] -> [a]) -> ([a] -> [a] -> [a])
 unwrap f = repH . f
 
@@ -10,10 +14,6 @@ wrap :: ([a] -> [a] -> [a]) -> ([a] -> [a])
 wrap g = absH . g
 
 {-# RULES "ww" forall f. fix f = wrap (fix (unwrap . f . wrap)) #-}
-
--- rev :: [a] -> [a]
-rev []     = []
-rev (x:xs) = rev xs ++ [x]
 
 main :: IO ()
 main = putStrLn $ "Successfully reversed a list of "
