a66e2f28c6ffb9d3d9490e9fcdec9836b8a6d25d
Neil Sculthorpe <neil@ittc.ku.edu>
2012-08-16 19:21:10 -0500

    Added "binding-group-of" command. Changed "consider" to focus on the definition in the recursive case (instead of the binding group).

--- src/Language/HERMIT/Primitive/Navigation.hs
+++ src/Language/HERMIT/Primitive/Navigation.hs
@@ -1,4 +1,15 @@
-module Language.HERMIT.Primitive.Navigation where
+module Language.HERMIT.Primitive.Navigation
+       ( -- * Navigation
+         externals
+       , bindGroup
+       , namedBinding
+       , bindingGroupOf
+       , considerName
+       , rhsOf
+       , considerables
+       , considerTargets
+       )
+where
 
 import GhcPlugins as GHC
 
@@ -12,43 +23,46 @@ import Data.Monoid
 
 import qualified Language.Haskell.TH as TH
 
+-- | 'External's involving navigating to named entities.
 externals :: [External]
 externals = map (.+ Navigation)
             [
               external "consider" considerName
-                [ "consider '<v> focuses on a named binding <v>" ]
+                [ "consider '<v> focuses on the definition of <v>" ]
             , external "consider" considerConstruct
                 [ "consider <c> focuses on the first construct <c>.",
                   recognizedConsiderables]
             , external "rhs-of" rhsOf
-                [ "rhs-of 'name focuses into the right-hand-side of binding <v>" ]
+                [ "rhs-of '<v> focuses on the right-hand-side of the definition of <v>" ]
+            , external "binding-group-of" bindingGroupOf
+                [ "binding-group-of '<v> focuses on the binding group that binds the variable <v>" ]
             ]
 
--- focus on a binding group
-considerName :: TH.Name -> TranslateH Core Path
-considerName = oneNonEmptyPathToT . bindGroup
+-- | Find the path to the RHS of the binding group of the given name.
+bindingGroupOf :: TH.Name -> TranslateH Core Path
+bindingGroupOf = oneNonEmptyPathToT . bindGroup
 
--- find a binding group that contains a name.
--- this is slightly different than namedBinding below,
--- as it will take us to the rec in a let rec, rather than
--- the actual binding. TODO: modify considerName or rhsOf
--- so we only need one of these?
+-- | Find the path to the definiiton of the provided name.
+considerName :: TH.Name -> TranslateH Core Path
+considerName = oneNonEmptyPathToT . namedBinding
+
+-- | Find the path to the RHS of the definition of the given name.
+rhsOf :: TH.Name -> TranslateH Core Path
+rhsOf nm = onePathToT (namedBinding nm) >>^ (++ [0])
+
+-- | Verify that this is a binding group defining the given name.
 bindGroup :: TH.Name -> Core -> Bool
 bindGroup nm (BindCore (NonRec v _))  =  nm `cmpTHName2Id` v
 bindGroup nm (BindCore (Rec bds))     =  any (cmpTHName2Id nm . fst) bds
 bindGroup _  _                        =  False
 
--- find a specific binding's rhs.
-rhsOf :: TH.Name -> TranslateH Core Path
-rhsOf nm = onePathToT (namedBinding nm) >>^ (++ [0])
-
--- find a named binding.
+-- | Verify that this is the definition of the given name.
 namedBinding :: TH.Name -> Core -> Bool
 namedBinding nm (BindCore (NonRec v _))  =  nm `cmpTHName2Id` v
 namedBinding nm (DefCore (Def v _))      =  nm `cmpTHName2Id` v
 namedBinding _  _                        =  False
 
--- find all the possible targets of consider
+-- | Find all the possible targets of \"consider\".
 considerTargets :: TranslateH Core [String]
 considerTargets = allT (collectT (promoteT $ nonRec <+ rec)) >>> arr concat
     where nonRec = nonRecT mempty (\ v () -> [unqualifiedIdName v])
@@ -60,6 +74,7 @@ data Considerable = Binding | Definition | CaseAlt | Variable | Literal | Applic
 recognizedConsiderables :: String
 recognizedConsiderables = "Recognized constructs are: " ++ show (map fst considerables)
 
+-- | Lookup table for constructs that can be considered; the keys are the arguments the user can give to the \"consider\" command.
 considerables ::  [(String,Considerable)]
 considerables =   [ ("bind",Binding)
                   , ("def",Definition)
