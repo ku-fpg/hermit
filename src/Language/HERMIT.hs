2b12e56c107ceacf726720b4aa8df16733baba03
Andrew Farmer <anfarmer@ku.edu>
2012-08-24 20:22:18 -0500

    Remove dead modules.

--- src/Language/HERMIT.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE KindSignatures, GADTs, TemplateHaskell #-}
-module Language.HERMIT
-        ( DSL.H                     -- abstact
-        , DSL.apply                 -- DSL
-        , DSL.consider              -- DSL
-        ) where
-
-import Language.HERMIT.DSL as DSL
