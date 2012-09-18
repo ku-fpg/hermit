c53ee4029298e82d9b50e53aca315d5ffdab5f13
Neil Sculthorpe <neil@ittc.ku.edu>
2012-09-18 16:53:02 -0500

    Started work on evaluation example.

---
+++ examples/evaluation/Eval.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Data.Function(fix)
+
+data Expr = Val Int | Add Expr Expr | Throw | Catch Expr Expr
+
+type Mint = Maybe Int
+
+eval :: Expr -> Mint
+eval (Val n)     = Just n
+eval Throw       = Nothing
+eval (Catch x y) = case eval x of
+                       Nothing -> eval y
+                       Just n  -> Just n
+eval (Add x y)   = case eval x of
+                       Nothing -> Nothing
+                       Just m  -> case eval y of
+                                    Nothing -> Nothing
+                                    Just n  -> Just (m + n)
+
+{-# NOTRULES "ww" forall f . fix f = wrap (fix (unwrap . f . wrap)) #-}
+
+main :: IO ()
+main = print (eval $ Val 5)
