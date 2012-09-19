e3bede383300b187bd09ce9382f5286758fcc40f
Neil Sculthorpe <neil@ittc.ku.edu>
2012-09-19 11:09:34 -0500

    Added "mean" example.

---
+++ examples/mean/Mean.hs
@@ -0,0 +1,20 @@
+import Prelude hiding (sum, length, foldr)
+
+-- so we can let-tuple
+import GHC.Tuple
+
+{-# NOINLINE mean #-}
+mean :: [Int] -> Int
+mean xs = sum xs `div` length xs
+
+sum :: [Int] -> Int
+sum xs = foldr (+) 0 xs
+
+length :: [Int] -> Int
+length xs = foldr (const $ (+) 1) 0 xs
+
+foldr f z [] = z
+foldr f z (x:xs) = x `f` foldr f z xs
+
+main :: IO ()
+main = print $ mean [1..10]
