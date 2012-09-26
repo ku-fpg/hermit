62b8e0580fc0a203d171b26eefca342825684b1a
Neil Sculthorpe <neil@ittc.ku.edu>
2012-09-25 22:02:27 -0500

    Started the boxing/unboxing example (factorial).

---
+++ examples/factorial/Fac.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE MagicHash #-}
+
+import GHC.Exts
+import Data.Function(fix)
+
+fac :: Int -> Int
+fac 0 = 1
+fac n = n * fac (n -1)
+
+unwrap :: (Int -> Int) -> Int# -> Int#
+unwrap h x = case h (I# x) of
+               I# y -> y
+
+wrap :: (Int# -> Int#) -> Int -> Int
+wrap h (I# x) = I# (h x)
+
+{-# RULES "ww"     forall f. fix f = wrap (fix (unwrap . f . wrap)) #-}
+
+main :: IO ()
+main = print (fac 50)
