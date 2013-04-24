d9cf9bfc69094394a42a61fbb067e2e1b8665170
Neil Sculthorpe <neil@ittc.ku.edu>
2013-04-24 15:45:05 -0500

    Added new module for syntactic utilities.

---
+++ src/Language/HERMIT/Syntax.hs
@@ -0,0 +1,27 @@
+module Language.HERMIT.Syntax
+   (
+      -- * Syntax Utilities
+      isIdFirstChar,
+      isIdChar,
+      isInfixId
+   )
+where
+
+import Data.Char (isAlphaNum)
+
+---------------------------------------------------------------------
+
+-- | Chars that are valid in identifiers anywhere.
+isIdFirstChar :: Char -> Bool
+isIdFirstChar c = isAlphaNum c || c `elem` "$_:."
+
+-- | Chars that are valid in identifiers, but not as the first character.
+isIdChar :: Char -> Bool
+isIdChar c = isIdFirstChar c || c `elem` "-'"
+
+-- | Chars that are valid in infix operators.
+isInfixId :: Char -> Bool
+isInfixId c = c `elem` "!£$%^&*-+=@#<>?/.:|"
+              -- I removed _ ' \\  as I don't think they're valid infix-operator symbols.
+
+---------------------------------------------------------------------
