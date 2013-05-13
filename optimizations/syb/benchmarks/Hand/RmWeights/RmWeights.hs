d7fc8df92dc6f5b2aa494ad71ce441aaf69bf9fa
Andrew Farmer <anfarmer@ku.edu>
2013-05-13 13:25:43 -0700

    Move SYB optimization out to own repository.

--- optimizations/syb/benchmarks/Hand/RmWeights/RmWeights.hs
+++
@@ -1,8 +0,0 @@
-module Hand.RmWeights.RmWeights where
-
-import TreeDatatype
-
-rmWeights :: WTree Int Int -> WTree Int Int
-rmWeights (WithWeight t w) = rmWeights t
-rmWeights (Leaf x)         = Leaf x
-rmWeights (Fork l r)       = Fork (rmWeights l) (rmWeights r)
