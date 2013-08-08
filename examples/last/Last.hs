e502ebe1d4ac598c831d05b68adcece297bbcc91
Neil Sculthorpe <neil@ittc.ku.edu>
2013-08-08 14:01:27 -0500

    Trying to proves WW Assumption B for "last".  Stuck due to bug(?) in fold.

--- examples/last/Last.hs
+++ examples/last/Last.hs
@@ -3,17 +3,31 @@ module Main where
 import Data.Function(fix)
 import Prelude hiding (last)
 
+-------------------------------------------------
+
 unwrap         :: ([a] -> a) -> a -> [a] -> a
 unwrap f a as  = f (a:as)
 
 wrap           :: (a -> [a] -> a) -> [a] -> a
-wrap f []      = error "wrap _ []"
+wrap f []      = undefined
 wrap f (a:as)  = f a as
 
 last           :: [a] -> a
-last []        = error "last []"
+last []        = undefined
 last [a]       = a
 last (_:a:as)  = last (a:as)
 
 main :: IO ()
 main = print (last "hello")
+
+-------------------------------------------------
+
+f :: ([a] -> a) -> [a] -> a
+f h []        = undefined
+f h [a]       = a
+f h (_:a:as)  = h (a:as)
+
+proof :: ([a] -> a) -> ([a] -> a)
+proof = \ h -> wrap (unwrap (f h))
+
+-------------------------------------------------
