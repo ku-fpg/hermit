d8eefc3d1ffff35ef2378ee968f7cefe44822408
Andrew Farmer <anfarmer@ku.edu>
2013-09-11 15:35:59 -0500

    Further generalize static-arg

--- src/HERMIT/Primitive/Function.hs
+++ src/HERMIT/Primitive/Function.hs
@@ -2,6 +2,7 @@
 module HERMIT.Primitive.Function
     ( externals
     , staticArgR
+    , staticArgPosR
     , staticArgPredR
     , staticArgTypesR
     )
@@ -9,7 +10,7 @@ where
 
 import Control.Arrow
 
-import Data.List(transpose)
+import Data.List (nub, intersect, transpose)
 
 import HERMIT.Context
 import HERMIT.Core
@@ -30,18 +31,27 @@ externals =
 
 ------------------------------------------------------------------------------------------------------
 
-staticArgR :: forall c. (ExtendPath c Crumb, AddBindings c) => Rewrite c HermitM CoreDef
-staticArgR = staticArgPredR (const True)
+-- | Traditional Static Argument Transformation
+staticArgR :: (ExtendPath c Crumb, AddBindings c) => Rewrite c HermitM CoreDef
+staticArgR = staticArgPredR (return . map fst)
 
-staticArgTypesR :: forall c. (ExtendPath c Crumb, AddBindings c) => Rewrite c HermitM CoreDef
-staticArgTypesR = staticArgPredR (isTyVar . snd)
+-- | Static Argument Transformation that only considers type arguments to be static.
+staticArgTypesR :: (ExtendPath c Crumb, AddBindings c) => Rewrite c HermitM CoreDef
+staticArgTypesR = staticArgPredR (return . map fst . filter (isTyVar . snd))
 
-staticArgPredR :: forall c. (ExtendPath c Crumb, AddBindings c) 
-           => ((Int, Var) -> Bool) -- ^ if an arg is static, apply predicate to position and binder
-                                   --   True = transform as if it is static
-                                   --   False = consider arg to be dynamic
-           -> Rewrite c HermitM CoreDef
-staticArgPredR pr = prefixFailMsg "static-arg failed: " $ do
+-- | Static Argument Transformations which requires that arguments in the given position are static.
+staticArgPosR :: (ExtendPath c Crumb, AddBindings c) => [Int] -> Rewrite c HermitM CoreDef
+staticArgPosR is' = staticArgPredR $ \ss' -> let is = nub is'
+                                                 ss = map fst ss'
+                                            in if is == (is `intersect` ss)
+                                               then return is
+                                               else fail $ "args " ++ show (filter (`notElem` ss) is) ++ " are not static."
+
+-- | Generalized Static Argument Transformation, which allows static arguments to be filtered.
+staticArgPredR :: (ExtendPath c Crumb, AddBindings c)
+               => ([(Int, Var)] -> HermitM [Int]) -- ^ given list of static args and positions, decided which to transform
+               -> Rewrite c HermitM CoreDef
+staticArgPredR decide = prefixFailMsg "static-arg failed: " $ do
     Def f rhs <- idR
     let (bnds, body) = collectBinders rhs
     guardMsg (notNull bnds) "rhs is not a function"
@@ -52,14 +62,21 @@ staticArgPredR pr = prefixFailMsg "static-arg failed: " $ do
         let argExprs = transpose callPats
             numCalls = length callPats
             -- ensure argument is present in every call (partial applications boo)
-            (ps,dbnds) = unzip [ (i,b) | (i,b,exprs) <- zip3 [0..] bnds $ argExprs ++ repeat []
-                                       , length exprs /= numCalls || isDynamic i b exprs
-                                       ]
+            allBinds = zip [0..] bnds
+            staticBinds = [ (i,b) | ((i,b),exprs) <- zip allBinds $ argExprs ++ repeat []
+                                  , length exprs == numCalls && isStatic b exprs ]
 
-            isDynamic i b []                      = not $ pr (i,b) -- all were static, so check predicate
-            isDynamic i b ((Var b'):es)           | b == b' = isDynamic i b es
-            isDynamic i b ((Type (TyVarTy v)):es) | b == v  = isDynamic i b es
-            isDynamic _ _ _                       = True             -- not a simple repass, so dynamic
+            isStatic _ []                      = True  -- all were static
+            isStatic b ((Var b'):es)           | b == b' = isStatic b es
+            isStatic b ((Type (TyVarTy v)):es) | b == v  = isStatic b es
+            isStatic _ _                       = False -- not a simple repass, so dynamic
+
+        chosen <- decide staticBinds
+        guardMsg (notNull chosen) "no arguments selected for transformation."
+        guardMsg (all (`elem` (map fst staticBinds)) chosen)
+            $ "args " ++ show (map fst staticBinds) ++ " are static, but " ++ show chosen ++ " were selected."
+
+        let (ps,dbnds) = unzip [ (i,b) | (i,b) <- allBinds, i `notElem` chosen ]
 
         wkr <- newIdH (var2String f ++ "'") (exprType (mkCoreLams dbnds body))
 
