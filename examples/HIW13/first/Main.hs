bfca6045bd79a2d92be1280cc5c7d19b6aa64852
Andrew Farmer <anfarmer@ku.edu>
2013-09-22 10:18:43 -0500

    Examples for HIW

---
+++ examples/HIW13/first/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+foo :: [Int] -> Int
+foo = foldl (+) 0 . map (+1)
+
+bar :: [Int] -> [Int]
+bar = map (*2)
+
+main :: IO ()
+main = print $ foo . bar $ [1..10]
+
+{-# RULES "map/map" [~] forall f g xs. map f (map g xs) = map (f . g) xs #-}
