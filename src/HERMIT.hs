c7dd827f2ec22c19f8425afe8fa1f96ce5f6ba26
Andrew Farmer <anfarmer@ku.edu>
2013-09-24 09:24:38 -0500

    Fix plugin to respect -p flag

--- src/HERMIT.hs
+++ src/HERMIT.hs
@@ -1,7 +1,12 @@
 module HERMIT (plugin) where
 
+import Data.List (isPrefixOf)
+
 import HERMIT.GHC
 import HERMIT.Optimize
 
 plugin :: Plugin
-plugin = optimize $ \ options -> phase 0 $ interactive [] options
+plugin = optimize $ \ options -> let pn = case filter ("-p" `isPrefixOf`) options of
+                                            [] -> 0
+                                            (('-':'p':num):_) -> read num
+                                 in phase pn $ interactive [] options
