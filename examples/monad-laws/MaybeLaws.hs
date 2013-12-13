ef2395b9abb636fb7456a87c0198694e825c9a83
Ed Komp <komp@ittc.ku.edu>
2013-12-12 19:47:21 -0600

    Proof for right unit law for Maybe monad

--- examples/monad-laws/MaybeLaws.hs
+++ examples/monad-laws/MaybeLaws.hs
@@ -2,6 +2,8 @@ module MaybeLaws where
 
 {-# RULES "left-unit"  forall x f.  retur x `bind` f  =  f x  #-}
 
+{-# RULES "right-unit"  forall m.   m `bind` retur  =  m  #-}
+
 bind :: Maybe a -> (a -> Maybe b) -> Maybe b
 bind Nothing  k = Nothing
 bind (Just a) k = k a
