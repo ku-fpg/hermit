2a227e2aaded3e39021fb01f1b1e9e3726280a7f
Andrew Farmer <anfarmer@ku.edu>
2014-04-07 12:14:53 -0500

    Make original_reverse example work again

--- examples/original_reverse/Reverse.hs
+++ examples/original_reverse/Reverse.hs
@@ -4,16 +4,16 @@ import Criterion.Main
 import HList
 import Data.Function (fix)
 
-{-# INLINE unwrap #-}
-unwrap :: ([a] -> [a]) -> ([a] -> H a)
-unwrap f = repH . f
+{-# INLINE repR #-}
+repR :: ([a] -> [a]) -> ([a] -> H a)
+repR f = repH . f
 
-{-# INLINE wrap #-}
-wrap :: ([a] -> H a) -> ([a] -> [a])
-wrap g = absH . g
+{-# INLINE absR #-}
+absR :: ([a] -> H a) -> ([a] -> [a])
+absR g = absH . g
 
-{-# RULES "ww" forall f . fix f = wrap (fix (unwrap . f . wrap)) #-}
-{-# RULES "inline-fix" forall f . fix f = let w = f w in w #-}
+{-# RULES "ww" forall body. fix body = absR (fix (repR . body . absR)) #-}
+-- {-# RULES "inline-fix" forall f . fix f = let w = f w in w #-}
 
 -- rev :: [a] -> [a]
 rev []     = []
