d26e19da432bc5e3e845a18f4c28351560cb5f62
Andrew Farmer <afarmer@ittc.ku.edu>
2014-07-01 17:24:10 -0500

    Add new reverse example using new w/w rewrites. Lemmas not yet proven.

---
+++ examples/new_reverse/Reverse.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Criterion.Main
+import HList
+import Data.Function (fix)
+
+{-# INLINE repR #-}
+repR :: ([a] -> [a]) -> ([a] -> H a)
+repR f = repH . f
+
+{-# INLINE absR #-}
+absR :: ([a] -> H a) -> ([a] -> [a])
+absR g = absH . g
+
+rev :: [a] -> [a]
+rev []     = []
+rev (x:xs) = rev xs ++ [x]
+
+main = defaultMain
+       [ bench (show n) $ whnf (\n -> sum $ rev [1..n]) n
+       | n <- take 8 $ [50,100..]
+       ]
