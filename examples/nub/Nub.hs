e97b2130a176dd0d9432aef9f4003169c7ad6c1b
Andrew Farmer <afarmer@ittc.ku.edu>
2014-07-03 11:39:32 -0500

    Mechanize nub example (finally) using new split-2-beta rewrite.

--- examples/nub/Nub.hs
+++ examples/nub/Nub.hs
@@ -1,24 +1,32 @@
+module Main where
+
 import Criterion.Main
 
 import qualified Data.Set as Set
 import Data.Set (Set)
 
-data R = R [Int] (Set Int)
+import Prelude hiding (filter) -- because we can't get unfolding for filter
 
-repR :: [Int] -> R
-repR xs = R xs Set.empty
-
-absR :: R -> [Int]
-absR (R xs ex) = [ x | x <- xs
-                     , not (x `Set.member` ex)
-                 ]
+filter :: (a -> Bool) -> [a] -> [a]
+filter _ [] = []
+filter p (x:xs) = if p x then x : filter p xs else filter p xs
 
 nub :: [Int] -> [Int]
 nub [] = []
-nub (x:xs) = x : nub (filter (\y -> x /= y) xs)
+nub (x:xs) = x : nub (filter (/= x) xs)
+
+absN :: ([Int] -> Set Int -> [Int]) -> [Int] -> [Int]
+absN h [] = []
+absN h (x:xs) = x : h xs (Set.singleton x)
+
+repN :: ([Int] -> [Int]) -> [Int] -> Set Int -> [Int]
+repN h xs s = h (filter (`Set.notMember` s) xs)
 
 --main = print (nub [ x | n <- [1..1000], x <- [1..n] ])
 main = defaultMain
         [ bench (show sz) $ nf nub [ x | n <- [1..sz], x <- [1..n] ]
         | sz <- take 2 [100,200..]
         ]
+
+{-# RULES "filter-fusion" [~] forall p q ys. filter p (filter q ys) = filter (\y -> p y && q y) ys #-}
+{-# RULES "member-fusion" [~] forall y x s. (y /= x) && (y `Set.notMember` s) = y `Set.notMember` (Set.insert x s) #-}
